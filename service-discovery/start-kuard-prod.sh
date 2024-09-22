a33d6d5e065cdb9a8338cafd7830ebd32ed8b57e
ralph <ralphexp@example.com>
2024-09-22 11:41:23 -0400

    update

--- service-discovery/start-kuard-prod.sh
+++ service-discovery/start-kuard-prod.sh
@@ -5,11 +5,16 @@ echo "creating deployment ..."
 microk8s kubectl create deployment kuard \
     --image=kuard:local \
     --port=8080 \
+    --replicas=2 && \
+microk8s kubectl create deployment kuard2 \
+    --image=kuard:local \
+    --port=8080 \
     --replicas=2
-
+  
 if [ $? == 0 ]; then
 # expose the deployment
-    microk8s kubectl expose deployment kuard
+    microk8s kubectl expose deployment kuard &&
+    microk8s kubectl expose deployment kuard2
 else
     exit 1;
 fi
--- service-discovery/stop-kuard-prod.sh
+++ service-discovery/stop-kuard-prod.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 echo "deleting kuard..."
-microk8s kubectl delete services,deployments kuard
+microk8s kubectl delete services,deployments kuard && \
+microk8s kubectl delete services,deployments kuard2
+
 if [ $? == 0 ]; then
   echo "done"
 fi
